ae0fa635276b685367a41476c0768a54ab71807f
Abel Cobreros <info@abelcobreros.com>
2025-03-16 13:44:08 +0100

    Move request to a specific DataTableRequest class

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DatatableRequest;
 use App\Queries\UserQueries;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response as InertiaResponse;
 
@@ -12,14 +12,14 @@ class UserController extends Controller
     /**
      * Show the list of users
      */
-    public function index(Request $request, UserQueries $userQueries): InertiaResponse
+    public function index(DatatableRequest $request, UserQueries $userQueries): InertiaResponse
     {
-        $page = $request->input('page', 1);
-        $perPage = $request->input('perPage', 10);
-        $sort = $request->input('sort');
-        $order = $request->input('order', 'asc');
-
-        $users = $userQueries->getPaginatedUsers($page, $perPage, $sort, $order);
+        $users = $userQueries->getPaginatedUsers(
+            $request->getPage(),
+            $request->getPerPage(),
+            $request->getSort(),
+            $request->getOrder()
+        );
 
         return Inertia::render('users/index', [
             'users' => $users->items(),
@@ -27,8 +27,8 @@ class UserController extends Controller
             'perPage' => $users->perPage(),
             'total' => $users->total(),
             'lastPage' => $users->lastPage(),
-            'sort' => $sort,
-            'order' => $order,
+            'sort' => $request->getSort(),
+            'order' => $request->getOrder(),
         ]);
     }
 }
